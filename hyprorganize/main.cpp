753ae3e41e46676b09a24f7c47197fae02a83dbb
Uyanide <pywang0608@foxmail.com>
2025-08-04 21:37:37 +0200

    fix: register new worksapces after creating them

--- hyprorganize/main.cpp
+++ hyprorganize/main.cpp
@@ -1,3 +1,9 @@
+/*
+ * @Author: Uyanide pywang0608@foxmail.com
+ * @Date: 2025-08-04 20:26:06
+ * @LastEditTime: 2025-08-04 21:36:42
+ * @Description: Hyprland plugin to organize workspaces
+ */
 #define WLR_USE_UNSTABLE
 
 #include <unistd.h>
@@ -25,94 +31,6 @@ inline HANDLE PHANDLE = nullptr;
 // Do NOT change this function.
 APICALL EXPORT std::string PLUGIN_API_VERSION() {
     return HYPRLAND_API_VERSION;
-}
-
-static SDispatchResult moveOrExec(std::string in) {
-    CVarList vars(in, 0, ',');
-
-    if (!g_pCompositor->m_lastMonitor || !g_pCompositor->m_lastMonitor->m_activeWorkspace)
-        return SDispatchResult{.success = false, .error = "No active workspace"};
-
-    const auto PWINDOW = g_pCompositor->getWindowByRegex(vars[0]);
-
-    if (!PWINDOW)
-        g_pKeybindManager->spawn(vars[1]);
-    else {
-        if (g_pCompositor->m_lastMonitor->m_activeWorkspace != PWINDOW->m_workspace)
-            g_pCompositor->moveWindowToWorkspaceSafe(PWINDOW, g_pCompositor->m_lastMonitor->m_activeWorkspace);
-        else
-            g_pCompositor->warpCursorTo(PWINDOW->middle());
-        g_pCompositor->focusWindow(PWINDOW);
-    }
-
-    return SDispatchResult{};
-}
-
-static SDispatchResult throwUnfocused(std::string in) {
-    const auto [id, name] = getWorkspaceIDNameFromString(in);
-
-    if (id == WORKSPACE_INVALID)
-        return SDispatchResult{.success = false, .error = "Failed to find workspace"};
-
-    if (!g_pCompositor->m_lastWindow || !g_pCompositor->m_lastWindow->m_workspace)
-        return SDispatchResult{.success = false, .error = "No valid last window"};
-
-    auto pWorkspace = g_pCompositor->getWorkspaceByID(id);
-    if (!pWorkspace)
-        pWorkspace = g_pCompositor->createNewWorkspace(id, g_pCompositor->m_lastWindow->m_workspace->monitorID(), name, false);
-
-    for (const auto& w : g_pCompositor->m_windows) {
-        if (w == g_pCompositor->m_lastWindow || w->m_workspace != g_pCompositor->m_lastWindow->m_workspace)
-            continue;
-
-        g_pCompositor->moveWindowToWorkspaceSafe(w, pWorkspace);
-    }
-
-    return SDispatchResult{};
-}
-
-static SDispatchResult bringAllFrom(std::string in) {
-    const auto [id, name] = getWorkspaceIDNameFromString(in);
-
-    if (id == WORKSPACE_INVALID)
-        return SDispatchResult{.success = false, .error = "Failed to find workspace"};
-
-    if (!g_pCompositor->m_lastMonitor || !g_pCompositor->m_lastMonitor->m_activeWorkspace)
-        return SDispatchResult{.success = false, .error = "No active monitor"};
-
-    auto pWorkspace = g_pCompositor->getWorkspaceByID(id);
-    if (!pWorkspace)
-        return SDispatchResult{.success = false, .error = "Workspace isnt open"};
-
-    const auto PLASTWINDOW = g_pCompositor->m_lastWindow.lock();
-
-    for (const auto& w : g_pCompositor->m_windows) {
-        if (w->m_workspace != pWorkspace)
-            continue;
-
-        g_pCompositor->moveWindowToWorkspaceSafe(w, g_pCompositor->m_lastMonitor->m_activeWorkspace);
-    }
-
-    if (PLASTWINDOW) {
-        g_pCompositor->focusWindow(PLASTWINDOW);
-        g_pCompositor->warpCursorTo(PLASTWINDOW->middle());
-    }
-
-    return SDispatchResult{};
-}
-
-static SDispatchResult closeUnfocused(std::string in) {
-    if (!g_pCompositor->m_lastMonitor)
-        return SDispatchResult{.success = false, .error = "No focused monitor"};
-
-    for (const auto& w : g_pCompositor->m_windows) {
-        if (w->m_workspace != g_pCompositor->m_lastMonitor->m_activeWorkspace || w->m_monitor != g_pCompositor->m_lastMonitor || !w->m_isMapped || w == g_pCompositor->m_lastWindow)
-            continue;
-
-        g_pCompositor->closeWindow(w);
-    }
-
-    return SDispatchResult{};
 }
 
 static SDispatchResult organizeworkspaces(std::string in) {
@@ -147,6 +65,7 @@ static SDispatchResult organizeworkspaces(std::string in) {
             if (!_workspaceTo) {
                 return SDispatchResult{.success = false, .error = "Failed to create workspace"};
             }
+            g_pCompositor->registerWorkspace(_workspaceTo);
         }
         for (const auto _window : g_pCompositor->m_windows) {
             if (_window->m_workspace == _workspaceFrom) {
